e3ff68f5c7c86eeba3d03065971bbb5dda0df53f
harshad <harshadshirwadkar@gmail.com>
2015-01-04 03:00:54 -0500

    Implemented todo_list, todo_add, todo_export

--- todo.py
+++ todo.py
@@ -1,12 +1,13 @@
 #!/bin/python3
 import Orgnode
+import sys
 import os
 
 ##
 ## Locate correct TODO file
 ##
 def locate_todofile():
-    print (os.getcwd())
+    return "%s/.org" % os.getcwd()
     ## Set global variable pointing to correct todofile
 
 def print_chars_lvl(lvl, char):
@@ -14,22 +15,70 @@ def print_chars_lvl(lvl, char):
         print(char, end="")
         lvl = lvl - 1
 
-def dump(nodelist):
-    id = 1
-    for node in nodelist:
-        print_chars_lvl(node.level - 1, '\t')
-        print ("[" + str(id) + "] " + node.Heading())
-        id = id + 1
-
 def export(nodelist):
     for node in nodelist:
         print_chars_lvl(node.level, '*')
         print (" " + "[" + node.Priority() + "] " + node.Todo() + " " + node.Heading())
         print (node.Body())
 
+
+def todo_init(todofile):
+    if os.path.isfile(todofile):
+        print("File exists")
+    else:
+        print("File does not exist")
+
+def todo_list(nodelist):
+    id = 1
+    for node in nodelist:
+        print_chars_lvl(node.level - 1, '\t')
+        print ("[" + str(id) + "] " + node.Heading())
+        id = id + 1
+
+def todo_add(nodelist):
+    print("--")
+    todo_list(nodelist)
+
+    #    node.Heading= input('Nest_under: ')
+    # index = input('Index: ')
+    # headline = input('Headline: ')
+    # body = input('Body: ')
+    # level = input('Level: ')
+
+    index = 6
+    headline = "hello"
+    body = "world"
+    level = "**"
+    priority = "A"
+    todo = "TODO"
+
+    node = Orgnode.Orgnode(level, headline, body, '', '')
+    node.todo = todo
+    node.prty = priority
+
+    nodelist.insert(int(index), node)
+    todo_list(nodelist)
+    export(nodelist)
 ##
 ## main()
 ##
+
+orgfile = "./myorg.org"
+if sys.argv[1] == "init":
+    orgfile = "%s/.org" % os.getcwd()
+    todo_init(orgfile)
+    exit(0)
+
+todofile = locate_todofile()
+nodelist = Orgnode.makelist(orgfile)
+
+if sys.argv[1] == "list":
+    todo_list(nodelist)
+if sys.argv[1] == "add":
+    todo_add(nodelist)
+else:
+    print(sys.argv[1] + ": Unsupported option.")
+
 todofile = locate_todofile()
 
 ##
@@ -48,10 +97,8 @@ todofile = locate_todofile()
 #### Search. display matching items.
 ##
 
-orgfile = "./myorg.org"
-nodelist = Orgnode.makelist(orgfile)
 
-dump(nodelist)
-export(nodelist)
-node1 = Orgnode.Orgnode("*","Item99","Item99-Body","","")
+# dump(nodelist)
+# export(nodelist)
+# node1 = Orgnode.Orgnode("*","Item99","Item99-Body","","")
 
